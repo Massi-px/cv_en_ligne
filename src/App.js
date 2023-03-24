0e5e3ac624ca15b9d92920f224bf140e66881e87
Massi-px <99023676+Massi-px@users.noreply.github.com>
2023-03-24 09:27:16 +0100

    first page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/img/logo.png';
+import './assets/css/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <ul className="App-menu">
+              <li className="App-onglet">
+                  <a href="accueil.html">Accueil</a>
+              </li>
+              <li className="App-onglet">
+                  <a href="films.html">Compétence</a>
+              </li>
+              <li className="App-onglet">
+                  <a href="musiques.html">Expérience</a>
+              </li>
+              <li className="App-onglet">
+                  <a href="musiques.html">Hobbie</a>
+              </li>
+          </ul>
       </header>
+
+      <body className="App-body">
+
+      </body>
+
     </div>
   );
 }
